8fc8788240631ccf87d4944b814c076780087eef
Rouh <roaming.new@gmail.com>
2021-01-28 00:55:47 +0900

    add sample

---
+++ pages/sample/hello/_id.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Hello, {{ this.id }}</h1>
+</template>
+
+<script>
+export default {
+    async asyncData({ params }){
+        const id = params.id
+        return { id }
+    }
+}
+</script>
