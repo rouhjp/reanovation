cdc173e692995244386e434240b0e16d0c19ac7c
Rouh <roaming.new@gmail.com>
2021-01-29 00:12:43 +0900

    add helloWorld Function FireBase

--- pages/sample/hello/_id.vue
+++ pages/sample/hello/_id.vue
@@ -1,12 +1,21 @@
 <template>
-    <h1>Hello, {{ this.id }}</h1>
+    <h1>{{ this.greeting }}, {{ this.id }} {{ this.ps }}</h1>
 </template>
 
 <script>
 export default {
     async asyncData({ params }){
+        const greeting = "Hello"
         const id = params.id
-        return { id }
+        let message = ""
+        if(id==="legoyoshi"){
+            message = "I LOVE YOU"
+        }
+        return { 
+            greeting: greeting,
+            id,
+            ps: message
+         }
     }
 }
 </script>
